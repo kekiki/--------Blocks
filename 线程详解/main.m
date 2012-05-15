99665fd94eaf4db7741b0534252c14ec281e2410
mac <mac@xpfdeiMac.local>
2012-05-15 10:08:53 +0800

    Initial Commit

---
+++ 线程详解/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  线程详解
+//
+//  Created by qwer qwerqw on 12-5-15.
+//  Copyright (c) 2012年 wer. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "AppDelegate.h"
+
+int main(int argc, char *argv[])
+{
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
